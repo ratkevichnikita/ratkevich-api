b6c9caeb3aa23d47726c6f8ff6bf42226fe72586
nikita <nikita.ratkevich91@mail.ru>
2025-01-13 21:08:07 +0200

    test deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Все настроено!');
+  res.send('Все настроено! Проверка автодеплоя');
 });
 
 app.listen(port, () => {
