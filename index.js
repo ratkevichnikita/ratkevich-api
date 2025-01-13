5cd114f4ba300d00a44e60f74b764535d01c9123
nikita <nikita.ratkevich91@mail.ru>
2025-01-13 21:18:17 +0200

    install cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+
+// Разрешаем запросы с http://localhost:3000
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.get('/', (req, res) => {
   res.send('Все настроено! Проверка автодеплоя');
 });
 
-app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Сервер запущен на http://localhost:3000');
 });
