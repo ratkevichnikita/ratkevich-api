89f81a8568f0ba96fb8c3e7d1e65b350dd9ed0b5
nikita <nikita.ratkevich91@mail.ru>
2025-01-14 20:50:27 +0200

    update2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-  from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -13,8 +13,8 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-return {"message": "Все настроено! Проверка автодеплоя"}
+    return {"message": "Все настроено! Проверка автодеплоя"}
 
 if __name__ == "__main__":
-import uvicorn
-uvicorn.run(app, host="0.0.0.0", port=3000)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3000)
